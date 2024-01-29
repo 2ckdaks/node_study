83061c0a48c5186a2e634fbb11a6b8d0f22d4276
이창민 <2ckdaks@gmail.com>
2024-01-29 23:25:02 +0900

    간단한 퍼블리싱

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static(__dirname + "/public")); //public 폴더내 등록된 파일 사용가능
+
 app.listen(8080, () => {
   console.log("http://localhost:8080 에서 서버 실행중");
 });

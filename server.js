0d9aad921ad67e8520cf8f6aab8d7698fe87fff7
이창민 <2ckdaks@gmail.com>
2024-01-30 23:05:11 +0900

    글 작성/db저장 기능  구현

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.use(express.static(__dirname + "/public")); //public 폴더내 등록된 파
 //환경변수 셋팅
 require("dotenv").config();
 
+//client에서 보낸 데이터를 서버에서 출력을위한 셋팅
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 //mongodb 셋팅
 const { MongoClient } = require("mongodb");
 let db;
@@ -36,4 +40,17 @@ app.get("/shop", (req, res) => {
 app.get("/list", async (req, res) => {
   let result = await db.collection("post").find().toArray();
   res.render("list.ejs", { result: result });
+});
+
+app.get("/write", (req, res) => {
+  res.render("write.ejs");
+});
+
+//client에서 전송받은 데이터를 db에 저장
+app.post("/write", async (req, res) => {
+  console.log(req.body);
+  await db
+    .collection("post")
+    .insertOne({ title: req.body.title, content: req.body.content });
+  res.send("success");
 });
